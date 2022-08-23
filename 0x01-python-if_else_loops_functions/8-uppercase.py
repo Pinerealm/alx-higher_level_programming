1f94eb59dc3283684952fd1e19dcc0ec69f680b1
Pinerealm <osalami09@gmail.com>
2022-08-23 21:41:17 +0300

    Prints a string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        islower = ord(c) >= ord('a') and ord(c) <= ord('z')
+        c = chr(ord(c) - 32 if islower else ord(c))
+        print(c, end='')
+    print()
