462c054a796229ab43728bbbe2a9c552d7feebe6
Pinerealm <osalami09@gmail.com>
2022-08-24 08:13:50 +0300

    Refactored code

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,7 +1,5 @@
 #include "lists.h"
 
-listint_t *add_nodeint(listint_t **head, const int n);
-
 /**
  * insert_node - inserts a number into a sorted singly linked list
  *
@@ -19,8 +17,17 @@ listint_t *insert_node(listint_t **head, int number)
 	current = *head;
 	if (current == NULL)
 		return (add_nodeint_end(head, number));
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
 	if (current->n >= number)
-		return (add_nodeint(head, number));
+	{
+		new->n = number;
+		new->next = current;
+		*head = new;
+		return (new);
+	}
 	while (current->n <= number)
 	{
 		if (current->next == NULL)
@@ -29,34 +36,9 @@ listint_t *insert_node(listint_t **head, int number)
 		current = current->next;
 	}
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
 	new->n = number;
 	new->next = current;
 	previous->next = new;
-
-	return (new);
-}
-
-/**
- * add_nodeint - adds a new node at the beginning of a listint_t list
- *
- * @head: address to the head pointer of the list
- * @n: integer to be added to the new node
- *
- * Return: address of the new node or NULL if it failed
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
 
 	return (new);
 }
