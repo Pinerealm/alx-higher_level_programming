c3fb279a3c7014fb02fe59ead571953e36b9a406
Pinerealm <osalami09@gmail.com>
2022-09-01 02:24:56 +0300

    Refactoring: edge case; empty list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def weight_average(my_list):
     total = 0
+    if len(my_list) == 0:
+        return total
     for i in my_list:
         total += i[0] * i[1]
     return total / sum(i[1] for i in my_list)
