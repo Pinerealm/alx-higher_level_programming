b9a9b4b7e7081a47c71a85ac68739e69c2428e7d
Pinerealm <osalami09@gmail.com>
2022-09-12 19:17:03 +0300

    Check when no elements are requested

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,6 +2,9 @@
 
 
 def safe_print_list(my_list=[], x=0):
+    if x == 0:
+        print()
+        return 0
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
