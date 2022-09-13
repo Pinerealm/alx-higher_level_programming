72a6330b35e02925452c73c4d7159a10fe6c897e
Pinerealm <osalami09@gmail.com>
2022-09-13 21:11:53 +0300

    Add module docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""This module contains a class called Square that defines a square."""
+
+
 class Square:
-    """This is the Square class.
-    """    
+    """This is the Square class definition."""
     pass
