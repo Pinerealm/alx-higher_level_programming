fedda0b2c4346a6c03b0d185bdaeac2b2498f284
Pinerealm <osalami09@gmail.com>
2022-09-19 20:23:45 +0300

    Prints text with 2 newlines after some characters

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module contains the text_indentation function"""
+
+
+def text_indentation(text):
+    """Prints a text with 2 new lines after certain characters
+
+    These characters are: '.', '?' and ':'
+
+    Args:
+        text (str): The text to print
+
+    Raises:
+        TypeError: If text is not a string
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for i in range(len(text)):
+        if text[i] == ' ' and text[i - 1] in '.?:':
+            continue
+        print(text[i], end="")
+        if text[i] in ".?:":
+            print("\n")
