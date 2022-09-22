22a93445feaa63e25fa63930ea936819c4a284f3
Pinerealm <osalami09@gmail.com>
2022-09-22 08:31:01 +0300

    Remove try/except block in fxn: my_print

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -38,14 +38,11 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            try:
-                for i in range(self.__position[1]):
-                    print()
-                for i in range(self.__size):
-                    print(" " * self.__position[0], end="")
-                    print("#" * self.__size)
-            except TypeError:
-                print("position must be a tuple of 2 positive integers")
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
 
     @property
     def size(self):
