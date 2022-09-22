f061159a965f0d3d05b2db571bc21807ceb6adce
Pinerealm <osalami09@gmail.com>
2022-09-22 05:57:42 +0300

    Remove whitespace

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,7 +5,7 @@
 class LockedClass:
     """The locked class"""
     __slots__ = ["first_name"]
-    
+
     def __init__(self, first_name=""):
         """The locked class constructor"""
         self.first_name = first_name
