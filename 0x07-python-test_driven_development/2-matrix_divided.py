6cb7111988dce4b560afb8dd326b8a3867470fcd
Pinerealm <osalami09@gmail.com>
2022-09-24 10:19:48 +0300

    Handle matrix values all resulting to zero

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -16,10 +16,9 @@ def matrix_divided(matrix, div):
         TypeError: if matrix is not a list of lists
         TypeError: if matrix contains non-integer or non-float elements
         TypeError: if div is not an integer or float
-        ZeroDivisionError: if div is 0
+        ZeroDivisionError: if div is 0 or less
+        OverflowError: if the divisor is too large
     """
-    if div == float('inf'):
-        raise OverflowError("divisor is too large")
     if isinstance(matrix, list) is False or len(matrix) == 0:
         raise TypeError("matrix must be a matrix (list of lists)"
                         " of integers/floats")
@@ -28,11 +27,14 @@ def matrix_divided(matrix, div):
                         " of integers/floats")
     if isinstance(div, (int, float)) is False:
         raise TypeError("div must be a number")
-    if div <= 0:
+    if div == 0:
         raise ZeroDivisionError("division by zero")
 
     new_matrix = []
     for row in matrix:
+        if isinstance(row, list) is False:
+            raise TypeError("matrix must be a matrix (list of lists)"
+                            " of integers/floats")
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
         new_row = []
@@ -42,4 +44,8 @@ def matrix_divided(matrix, div):
                                 " of integers/floats")
             new_row.append(round(element / div, 2))
         new_matrix.append(new_row)
+
+    if all([not all(row) for row in new_matrix]):
+        raise OverflowError("divisor is too large")
+
     return new_matrix
