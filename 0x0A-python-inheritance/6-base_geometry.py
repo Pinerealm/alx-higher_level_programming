669fe08228cefbb3f57a7b53acd046568cf6f9fa
Pinerealm <osalami09@gmail.com>
2022-09-27 08:00:21 +0300

    Create area method for BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""The BaseGeometry module"""
+
+
+class BaseGeometry:
+    """The BaseGeometry class"""
+
+    def area(self):
+        """The area method"""
+        raise Exception("area() is not implemented")
