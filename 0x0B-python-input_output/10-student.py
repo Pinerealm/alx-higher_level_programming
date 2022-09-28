247f6db2451acb5b67449594cc8efd6e344ffb77
Pinerealm <osalami09@gmail.com>
2022-09-28 23:01:41 +0300

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""The Student class module"""
+
+
+class Student:
+    """The Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes the Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the dictionary representation for JSON serialization
+        of an object
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {key: value for key, value in self.__dict__.items()
+                        if key in attrs}
+            return new_dict
