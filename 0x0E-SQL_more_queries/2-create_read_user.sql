5450e6b2f522120ac76173194a29320463543f27
Pinerealm <osalami09@gmail.com>
2022-11-02 20:02:04 +0300

    User can only read

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,11 @@
+-- Creates the database 'hbtn_0d_2' and user 'user_0d_2'.
+-- Create the database if it doesn't exist.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user if it doesn't exist.
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant 'select' privilege to the user.
+GRANT SELECT
+   ON hbtn_0d_2.*
+   TO 'user_0d_2'@'localhost';
