431df18b1e58206f434d8becac9163ad3bfcb7f1
Pinerealm <osalami09@gmail.com>
2022-11-05 12:04:00 +0300

    Convert table

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,3 +3,9 @@
 ALTER DATABASE hbtn_0c_0 
     CHARACTER SET utf8mb4 
     COLLATE utf8mb4_unicode_ci;
+
+-- Converts the table, 'hbtn_0c_0', to UTF8 with collation,
+-- 'utf8mb4_unicode_ci'
+ALTER TABLE first_table
+    CONVERT TO CHARACTER SET utf8mb4 
+    COLLATE utf8mb4_unicode_ci;
