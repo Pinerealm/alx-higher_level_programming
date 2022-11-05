345ddb4d8ceedb70005511d399538d34b9ed299f
Pinerealm <osalami09@gmail.com>
2022-11-05 13:47:08 +0300

    Cities of California

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- Lists all the cities in California in the database hbtn_0d_usa.
+SELECT *
+  FROM cities
+ WHERE state_id == (SELECT id
+                      FROM states
+                     WHERE name == 'California');
