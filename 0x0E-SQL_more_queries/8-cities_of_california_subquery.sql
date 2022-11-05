d83bdce7c6831b93fd01fcd763e603940cbe29a6
Pinerealm <osalami09@gmail.com>
2022-11-05 13:48:34 +0300

    Change to single equals sign

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,4 +3,4 @@ SELECT *
   FROM cities
  WHERE state_id == (SELECT id
                       FROM states
-                     WHERE name == 'California');
+                     WHERE name = 'California');
