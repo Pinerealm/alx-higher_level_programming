92d29ca1739a7ae70d51ad6fe2c5bec628f04bce
Pinerealm <osalami09@gmail.com>
2022-11-05 13:34:54 +0300

    Add not null constraint

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,6 +4,6 @@ USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS cities (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    state_id INT FOREIGN KEY(state_id) REFERENCES states(id),
+    state_id INT FOREIGN KEY(state_id) REFERENCES states(id) NOT NULL,
     name VARCHAR(256) NOT NULL
 );
