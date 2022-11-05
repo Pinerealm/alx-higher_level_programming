c83ffc993b35e70eb21b716bf0a8e8581beb6ac4
Pinerealm <osalami09@gmail.com>
2022-11-05 12:06:07 +0300

    Set database to use

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,6 @@
 -- Converts the database, 'hbtn_0c_0', to UTF8 with collation,
 -- 'utf8mb4_unicode_ci'
+USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 
     CHARACTER SET utf8mb4 
     COLLATE utf8mb4_unicode_ci;
