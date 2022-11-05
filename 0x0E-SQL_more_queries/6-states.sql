f8ae3f4d7ea3e7c85adb8f35848bd07a9bcd9084
Pinerealm <osalami09@gmail.com>
2022-11-05 13:26:21 +0300

    States to state

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
--- Creates a database, 'hbtn_0d_usa', and the tables, 'states'.
+-- Creates a database, 'hbtn_0d_usa', and the table, 'states'.
 -- 'states' has the columns, 'id' and 'name'.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
