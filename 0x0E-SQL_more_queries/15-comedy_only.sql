b1fe049e796e6b3a180d86cc4dce2fdc977d559a
Pinerealm <osalami09@gmail.com>
2022-11-07 08:31:50 +0100

    List comedy shows

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- List all comedy shows in the database
+SELECT title
+  FROM tv_shows AS s
+ INNER JOIN tv_show_genres AS sg
+    ON s.id = sg.show_id
+ INNER JOIN tv_genres AS g
+    ON sg.genre_id = g.id
+ WHERE g.name = 'Comedy'
+ ORDER BY title ASC;
