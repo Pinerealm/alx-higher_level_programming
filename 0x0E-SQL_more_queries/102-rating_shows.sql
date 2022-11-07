4918cd32e48277af8bbaed8b9ca6b29228f546b6
Pinerealm <osalami09@gmail.com>
2022-11-07 12:17:48 +0100

    Use inner join

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,9 +1,9 @@
 -- Lists all shows from the database, 'hbtn_0e_tvshows_rate', and their total rating.
 SELECT title, SUM(rate) AS rating
   FROM tv_shows AS s
-  LEFT JOIN tv_show_genres AS sg
+ INNER JOIN tv_show_genres AS sg
     ON s.id = sg.show_id
-  LEFT JOIN tv_show_ratings AS sr
-    ON s.id = sr.show_id
+ INNER JOIN tv_show_ratings AS sr
+    ON sg.show_id = sr.show_id
  GROUP BY title
  ORDER BY SUM(rate) DESC;
