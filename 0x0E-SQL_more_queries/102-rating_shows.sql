3a2f0febd3b69625dc01105c634aa3e5ffde7bd9
Pinerealm <osalami09@gmail.com>
2022-11-07 13:23:45 +0100

    Remove tv_show_genres table

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,9 +1,8 @@
--- Lists all shows from the database, 'hbtn_0e_tvshows_rate', and their total rating.
+-- Lists all shows from the database, 'hbtn_0d_tvshows_rate', 
+-- and their total rating.
 SELECT title, SUM(rate) AS rating
   FROM tv_shows AS s
- INNER JOIN tv_show_genres AS sg
-    ON s.id = sg.show_id
  INNER JOIN tv_show_ratings AS sr
-    ON sg.show_id = sr.show_id
+    ON s.id = sr.show_id
  GROUP BY title
  ORDER BY SUM(rate) DESC;
