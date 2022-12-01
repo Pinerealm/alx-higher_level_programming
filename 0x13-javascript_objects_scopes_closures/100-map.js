759893627f069db7294340ecc0b6fc30e60a5d2f
Pinerealm <osalami09@gmail.com>
2022-12-01 05:42:07 +0100

    Use array methos

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 
-function map (array, func) {
-  const newArray = [];
-  for (let i = 0; i < array.length; i++) {
-    newArray.push(func(array[i], i));
-  }
-  return newArray;
-}
+const newArray = list.map((x, index) => x * index);
 
-const newList = map(list, (x, i) => x * i);
 console.log(list);
-console.log(newList);
+console.log(newArray);
