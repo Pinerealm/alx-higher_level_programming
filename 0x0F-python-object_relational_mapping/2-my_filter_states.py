d1f447d2fef5825a9e408ae6c514b8f73e685b21
Pinerealm <osalami09@gmail.com>
2022-12-04 09:53:41 +0100

    Create query with str.format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id", (argv[4],))
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id".format(
+                                                                argv[4])
+    cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
         print(row)
