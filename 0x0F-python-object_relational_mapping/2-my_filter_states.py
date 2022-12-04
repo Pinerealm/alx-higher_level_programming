84f12d71a10736396ef43405001beec25dd59df3
Pinerealm <osalami09@gmail.com>
2022-12-04 22:40:22 +0100

    Modify doc to mention database name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This script displays all values in the states table of supplied database
+"""This script displays all values in the states table of hbtn_0e_0_usa
 where name matches the argument"""
 
 
