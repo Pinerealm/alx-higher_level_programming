6935e7690c019f01a522f985f9ae2c758438a366
Pinerealm <osalami09@gmail.com>
2022-12-04 23:12:27 +0100

    Define State class  and inherit declarative_base()

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module contains the State class which inherits from Base"""
+
+
+from sqlalchemy import Column, Integer, String
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """This class inherits from Base and links to the states table"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
