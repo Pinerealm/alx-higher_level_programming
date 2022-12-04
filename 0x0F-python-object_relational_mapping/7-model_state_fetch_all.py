ab60760ad435ada6eac1b4932ed5bbde1ca99777
Pinerealm <osalami09@gmail.com>
2022-12-05 00:08:23 +0100

    Use context manager

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    session = Session(engine)
-    for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+
+    with Session(engine) as session:
+        for state in session.query(State).order_by(State.id).all():
+            print("{}: {}".format(state.id, state.name))
