c1e650ec5915402277e3e08095acbe1e3d4de688
Pinerealm <osalami09@gmail.com>
2022-12-05 01:14:58 +0100

    Define class City

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module defines the City class for the hbtn_0e_14_usa database
+accessible via SQLAlchemy"""
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base, State
+
+
+class City(Base):
+    """This class inherits from Base and links to the cities table"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
