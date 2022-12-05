5e3174a8ab5b07d9c6be0c21a337058ae6d379dc
Pinerealm <osalami09@gmail.com>
2022-12-05 01:42:01 +0100

    Create state and city with one session

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This script creates the State “California” with the City “San Francisco”
+from the database hbtn_0e_100_usa"""
+
+
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        new_state = State(name="California")
+        new_state.cities = [City(name="San Francisco")]
+        session.add(new_state)
+        session.commit()
