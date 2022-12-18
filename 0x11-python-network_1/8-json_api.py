0ceaf39fb42677d2728f9d8d62a7ec45f46a04f8
Pinerealm <osalami09@gmail.com>
2022-12-18 23:50:12 +0300

    Use proper exception name

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -18,5 +18,5 @@ if __name__ == '__main__':
             print('[{}] {}'.format(json.get('id'), json.get('name')))
         else:
             print('No result')
-    except ValueError:
+    except requests.exceptions.JSONDecodeError:
         print('Not a valid JSON')
