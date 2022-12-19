20484d9e6f2402524c9a7cc06885e7edad6debd7
Pinerealm <osalami09@gmail.com>
2022-12-19 08:06:02 +0300

    Modify url

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""This script fetches 'https://intranet.hbtn.io/status'
+"""This script fetches 'https://alx-intranet.hbtn.io/status'
 via the package, requests"""
 import requests
 
 
 if __name__ == '__main__':
-    r = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
     print('\t- type: {}'.format(type(r.text)))
     print('\t- content: {}'.format(r.text))
