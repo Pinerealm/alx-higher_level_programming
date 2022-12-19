249f8c4e1f1bca8fa5c2e8ec8d802a6ff0e429e4
Pinerealm <osalami09@gmail.com>
2022-12-19 08:21:00 +0300

    Modify JSON check

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""This script takes in a letter as a parameter and sends a POST request to
-'http://0.0.0.0:5000/search_user'"""
+"""This script takes in a letter and sends a POST request to a server
+with the letter as a parameter. The server returns a JSON object"""
 import requests
 import sys
 
@@ -12,9 +12,10 @@ if __name__ == '__main__':
         q = ""
     url = 'http://689d9351c613.8f0453a5.alx-cod.online:5000/search_user'
     r = requests.post(url, data={'q': q})
+
     try:
         json = r.json()
-        if json and r.status_code == 200:
+        if json and r.raise_for_status() is None:
             print('[{}] {}'.format(json.get('id'), json.get('name')))
         else:
             print('No result')
