9caa0642fae1026a8f3b38bacab7385fa725d7bf
Pinerealm <osalami09@gmail.com>
2023-03-12 09:14:53 +0300

    Run if list is not empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for ele in reversed(my_list):
-        print("{:d}".format(ele))
+    if my_list:
+        for ele in reversed(my_list):
+            print("{:d}".format(ele))
