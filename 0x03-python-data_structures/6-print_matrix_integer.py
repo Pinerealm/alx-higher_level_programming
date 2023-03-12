81d88251ccdc9c873595b50908d126f21aaadcd2
Pinerealm <osalami09@gmail.com>
2023-03-12 09:23:30 +0300

    Print a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for ele in row:
+            print("{:d}".format(ele), end="")
+            if ele != row[-1]:
+                print(" ", end="")
+        print("")
