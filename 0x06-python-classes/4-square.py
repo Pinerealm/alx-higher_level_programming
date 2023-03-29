5fe3ae759322f61d65badc641c6cb46689ed6d2d
Pinerealm <osalami09@gmail.com>
2023-03-29 06:31:39 +0300

    Define a getter and setter for size

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""This is the square module."""
+
+
+class Square:
+    """This class defines a square."""
+
+    def __init__(self, size=0):
+        """Initialises a square instance.
+
+        Args:
+            size (int): Size of the square.
+        """
+        self.__size = size
+
+    def area(self):
+        """Calculate the area of the square.
+
+        Returns:
+            The area of the square.
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter method for the size attribute.
+
+        Returns:
+            The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method for the size attribute.
+
+        Args:
+            value (int): Size of the square.
+        """
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
