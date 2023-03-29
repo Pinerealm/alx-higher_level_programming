817a79a6483fe62ca4722a61cd142ee2a5ad0459
Pinerealm <osalami09@gmail.com>
2023-03-29 06:05:26 +0300

    Initialise an instance with private property, size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This is the square module."""
+
+
+class Square:
+    """This class defines a square."""
+
+    def __init__(self, size):
+        """Initialises a square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
