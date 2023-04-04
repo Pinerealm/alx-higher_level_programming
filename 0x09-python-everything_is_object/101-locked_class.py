b70eac9b3f2d836df53069ef8f1c67994515da05
Pinerealm <osalami09@gmail.com>
2023-04-04 22:58:02 +0300

    Specify the allowed attributes for a class

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is the LockedClass module"""
+
+
+class LockedClass:
+    """Defines a locked class with no class or object attribute.
+    
+    Attributes:
+        __slots__ (list): list of attributes that can be added to the object
+    """
+    __slots__ = ['first_name']
