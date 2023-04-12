834b8190cefaba8f0268c3927f413501246fbd67
Pinerealm <osalami09@gmail.com>
2023-04-12 20:19:58 +0300

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""The write_file module"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8)
+    
+    Args:
+        filename (str): The name of the file to write to
+        text (str): The string to write to the file
+        
+    Returns:
+        The number of characters written
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
