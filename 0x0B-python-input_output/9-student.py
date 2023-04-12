a0f58c28394a49baed250494037ef18c01470907
Pinerealm <osalami09@gmail.com>
2023-04-13 02:23:19 +0300

    Define a Student class with a to_json method

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""The Student module"""
+
+
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialises a student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns the dictionary representation, for JSON serialization,
+        of an object
+        """
+        return self.__dict__
