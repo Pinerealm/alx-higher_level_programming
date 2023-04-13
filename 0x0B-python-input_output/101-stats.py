c7db6d6d80184f3ca117221e521711557a193d1b
Pinerealm <osalami09@gmail.com>
2023-04-13 15:04:29 +0300

    Compute metrics from stdin

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""The 101-stats module"""
+import sys
+
+
+def print_stats(file_size, status_codes):
+    """Prints the stats"""
+    print(f'File size: {file_size}')
+    for key in sorted(status_codes):
+        print(f'{key}: {status_codes[key]}')
+
+if __name__ == "__main__":
+    line_count = 0
+    file_size = 0
+    status_codes = {}
+    try:
+        for line in sys.stdin:
+            line = line.rstrip()
+            line_count += 1
+            file_size += int(line.split()[-1])
+            code = line.split()[-2]
+
+            status_codes[code] = status_codes.get(code, 0) + 1
+            if line_count % 10 == 0:
+                print_stats(file_size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(file_size, status_codes)
+        raise
