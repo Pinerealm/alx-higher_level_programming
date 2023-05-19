917a65fd7e21d902c0f8a57dceacdc7c34f4f810
Pinerealm <osalami09@gmail.com>
2023-05-19 07:47:39 +0300

    Create a table with unique, auto-generated IDs

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Creates a database, 'hbtn_0d_usa', and the table, 'states'.
+-- 'states' has the columns, 'id' and 'name'.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL
+);
