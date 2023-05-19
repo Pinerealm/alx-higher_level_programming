d17c174b4e7b0ad7f1247dffa73e1fb2424d98fd
Pinerealm <osalami09@gmail.com>
2023-05-19 07:40:53 +0300

    Create a root user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Creates the user 'user_od_1'.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grants the user 'user_0d_1' all privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
