29985f6b65ddc45b9f6527238b0d820f433d9299
Pinerealm <osalami09@gmail.com>
2023-05-19 07:52:23 +0300

    List all cities in California from hbtn_0d_usa

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- Lists all the cities in California in the database hbtn_0d_usa.
+SELECT id, name
+  FROM cities
+ WHERE state_id = (SELECT id
+                     FROM states
+                    WHERE name = 'California');
+ ORDER BY id;
