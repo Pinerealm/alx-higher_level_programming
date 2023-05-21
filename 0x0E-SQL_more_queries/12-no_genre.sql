e2ee08aa638a979986e69d0f4e9e15f5e752f2e8
Pinerealm <osalami09@gmail.com>
2023-05-21 13:29:22 +0300

    Display shows with no genre_id

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- Lists all shows that have no genre in 'hbtn_0d_tvshows'.
+SELECT ts.title, tsg.genre_id
+  FROM tv_shows AS ts
+  LEFT JOIN tv_show_genres AS tsg
+    ON ts.id = tsg.show_id
+ WHERE tsg.genre_id IS NULL
+ ORDER BY ts.title, tsg.genre_id;
