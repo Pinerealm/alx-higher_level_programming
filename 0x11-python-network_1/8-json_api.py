0d108cb0eca4f44c183e9bb7a6aa5b865bb81201
Pinerealm <osalami09@gmail.com>
2023-07-03 07:05:10 +0300

    Search an API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Searches an API for a given string and displays the results.
+"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    q = sys.argv[1] if len(sys.argv) > 1 else ''
+    url = 'http://0.0.0.0:5000/search_user'
+    r = requests.post(url, data={'q': q})
+
+    try:
+        data = r.json()
+        if data:
+            print('[{}] {}'.format(data.get('id'), data.get('name')))
+        else:
+            print('No result')
+    except requests.exceptions.JSONDecodeError:
+        print('Not a valid JSON')
