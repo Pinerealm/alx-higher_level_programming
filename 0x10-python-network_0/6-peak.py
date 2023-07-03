e512d0666f774dd57d4a8362697b60deb22910f0
Pinerealm <osalami09@gmail.com>
2023-07-03 12:13:31 +0300

    Use a conditional instead of the max fxn

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,7 +11,8 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 1:
         return list_of_integers[0]
     if len(list_of_integers) == 2:
-        return max(list_of_integers)
+        return list_of_integers[0] if list_of_integers[0] >\
+            list_of_integers[1] else list_of_integers[1]
 
     mid = len(list_of_integers) // 2
     if list_of_integers[mid - 1] < list_of_integers[mid]\
