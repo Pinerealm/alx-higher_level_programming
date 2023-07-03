7306ef7cd10feb9d62828352949ebe48ae42cec9
Pinerealm <osalami09@gmail.com>
2023-07-03 07:33:30 +0300

    List the most recent 10 commits of the rails repo

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""List 10 commits (from the most recent to oldest) of the repository
+“rails” by the user “rails” from the GitHub API.
+"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    owner = sys.argv[2]
+    repo = sys.argv[1]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    r = requests.get(url)
+
+    try:
+        json = r.json()
+        for i in range(10):
+            print('{}: {}'.format(json[i].get('sha'),
+                                     json[i].get('commit').get('author')
+                                            .get('name')))
+    except requests.exceptions.JSONDecodeError:
+        print('Not a valid JSON')
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,7 +6,7 @@ import sys
 
 
 if __name__ == '__main__':
-    q = sys.argv[1] if len(sys.argv) > 1 else ''
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
     url = 'http://0.0.0.0:5000/search_user'
     r = requests.post(url, data={'q': q})
 
