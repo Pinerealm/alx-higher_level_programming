0929e44aab685452f6a7e7483d9ab7db649c7685
Pinerealm <osalami09@gmail.com>
2025-12-12 20:08:55 +0300

    Add zero-padding logic within the curly braces

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for i in range(100):
     delimiter = ", " if i < 99 else "\n"
-    if i < 10:
-        print('{}'.format(0), end="")
-    print('{}'.format(i), end=delimiter)
+    print('{:02}'.format(i), end=delimiter)
