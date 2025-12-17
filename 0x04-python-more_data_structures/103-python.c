cf0a89e8d9713bdd322d4aea598a3d51dfce2be7
Pinerealm <osalami09@gmail.com>
2025-12-17 17:25:57 +0300

    Remove unnecessary includes in 103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,12 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <assert.h>
-#include <pyport.h>
-#include <object.h>
-#include <bytesobject.h>
-#include <listobject.h>
+#include <Python.h>
 void print_python_bytes(PyObject *p);
 
 /**
