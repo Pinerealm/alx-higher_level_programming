b35aa3a8757594f08d4fdea770f5257a52f48975
Pinerealm <osalami09@gmail.com>
2025-12-18 20:38:12 +0300

    Use generator comprehensions in checks

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -24,9 +24,9 @@ def matrix_mul(m_a, m_b):
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
 
-    if not all([isinstance(row, list) for row in m_a]):
+    if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
-    if not all([isinstance(row, list) for row in m_b]):
+    if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
 
     if m_a == [] or m_a == [[]]:
@@ -34,14 +34,14 @@ def matrix_mul(m_a, m_b):
     if m_b == [] or m_b == [[]]:
         raise ValueError("m_b can't be empty")
 
-    if not all([isinstance(num, (int, float)) for row in m_a for num in row]):
+    if not all(isinstance(num, (int, float)) for row in m_a for num in row):
         raise TypeError("m_a should contain only integers or floats")
-    if not all([isinstance(num, (int, float)) for row in m_b for num in row]):
+    if not all(isinstance(num, (int, float)) for row in m_b for num in row):
         raise TypeError("m_b should contain only integers or floats")
 
-    if not all([len(row) == len(m_a[0]) for row in m_a]):
+    if not all(len(row) == len(m_a[0]) for row in m_a):
         raise TypeError("each row of m_a must be of the same size")
-    if not all([len(row) == len(m_b[0]) for row in m_b]):
+    if not all(len(row) == len(m_b[0]) for row in m_b):
         raise TypeError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
