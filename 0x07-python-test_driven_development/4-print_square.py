e4a2ee38a68c5aa032716730b36f14937d4c8dc0
Pinerealm <osalami09@gmail.com>
2025-12-18 18:50:37 +0300

    Remove redundant float and size check in print_square function

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -12,8 +12,6 @@ def print_square(size):
         TypeError: If size is not an integer.
         ValueError: If size is less than 0.
     """
-    if isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
